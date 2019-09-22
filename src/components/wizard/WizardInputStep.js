0e371f2f7b7c2c1f7ffe4a911c14becd0cb22727
Faizan Zahid <faizan.zahid88@gmail.com>
2019-09-23 00:07:00 +0500

    style of card

--- src/components/wizard/WizardInputStep.js
+++ src/components/wizard/WizardInputStep.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class WizardInputStep extends Component {
-
   constructor(props) {
     super(props);
-
 
     this.updateInputValue = this.updateInputValue.bind(this);
 
     this.state = {
-      value: '',
-    }
+      value: ""
+    };
   }
 
   updateInputValue(event) {
@@ -20,29 +18,44 @@ class WizardInputStep extends Component {
   }
 
   render() {
-
-    const { step } = this.props ;
+    const { step } = this.props;
 
     return (
-      <div className="container">
+      <div className="wizard-container">
         {this.props.step.title}
         <div className="row">
-        {!this.props.isFirstStep && <button onClick={() => this.props.onPrevious()}>Zurück</button> }
-      </div>
-      <div className="row">
-        <div className="col">
-              <img width="100" src={'svg/'+step.options[0].options.icon} alt={this.props.description}/>
-            </div>
+          {!this.props.isFirstStep && (
+            <button onClick={() => this.props.onPrevious()}>Zurück</button>
+          )}
+        </div>
+        <div className="row">
+          <div className="col-sm-8">
+            <img
+              width="100"
+              src={"svg/" + step.options[0].options.icon}
+              alt={this.props.description}
+            />
+          </div>
         </div>
         <div className="row">
           <div className="col">
-            {step.options[0].options.text}  <input value={this.state.inputValue} onChange={this.updateInputValue}/>
+            {step.options[0].options.text}{" "}
+            <input
+              value={this.state.inputValue}
+              onChange={this.updateInputValue}
+            />
           </div>
           <div className="col">
-              <button onClick={() => this.props.onNext(step.options[0], this.state.inputValue)}>Weiter</button>
+            <button
+              onClick={() =>
+                this.props.onNext(step.options[0], this.state.inputValue)
+              }
+            >
+              Weiter
+            </button>
           </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
