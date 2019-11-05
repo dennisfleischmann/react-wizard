750078d25d7728d4358a0f11cf6a3e3701bfa811
Dennis Fleischmann <dennisfleischmann@Denniss-MacBook-Pro.local>
2019-11-05 18:58:13 +0530

    Reduced the number of selections to max 5

--- src/apps/app-1/wizard-config.js
+++ src/apps/app-1/wizard-config.js
@@ -24,12 +24,11 @@ export default {
     "title": "Art des Gebäudes",
     "fieldName": "propertyType",
     "options": [
-      {  id:"0", value:"Einfamilienhaus", "next" : 2, "prev" : -1, "options": {"icon":"one_family_house.svg", "text":"Einfamilienhaus", "size":"s"}},
-      {  id:"1", value:"Reihenendhaus", "next" : 2, "prev" : -1, "options": {"icon":"row_house.svg", "text":"Reihenendhaus", "size":"s"}},
-      {  id:"2", value:"Reihenmittelhaus", "next" : 2, "prev" : -1, "options": {"icon":"row_house_middle.svg", "text":"Reihenmittelhaus", "size":"s"}},
-      {  id:"3", value:"Doppelhaushälfte", "next" : 2, "prev" : -1, "options": {"icon":"semi_detached_house.svg", "text":"Doppelhaushälfte", "size":"s"}},
-      {  id:"4", value:"Mehrfamilienhaus", "next" : 2, "prev" : -1, "options": {"icon":"multifamily_house.svg", "text":"Mehrfamilienhaus", "size":"s"}},
-      {  id:"5", value:"Wohn- & Geschäftshaus", "next" : 2, "prev" : -1, "options": {"icon":"live_commercial.svg", "text":"Wohn- & Geschäftshaus", "size":"s"}}
+      {  id:"0", value:"Einfamilienhaus", "next" : 2, "prev" : -1, "options": {"icon":"one_family_house.svg", "text":"Einfamilienhaus", "size":"m"}},
+      {  id:"1", value:"Reihenendhaus", "next" : 2, "prev" : -1, "options": {"icon":"row_house.svg", "text":"Reihenendhaus", "size":"m"}},
+      {  id:"2", value:"Doppelhaushälfte", "next" : 2, "prev" : -1, "options": {"icon":"semi_detached_house.svg", "text":"Doppelhaushälfte", "size":"m"}},
+      {  id:"3", value:"Mehrfamilienhaus", "next" : 2, "prev" : -1, "options": {"icon":"multifamily_house.svg", "text":"Mehrfamilienhaus", "size":"m"}},
+      {  id:"4", value:"Wohn- & Geschäftshaus", "next" : 2, "prev" : -1, "options": {"icon":"live_commercial.svg", "text":"Wohn- & Geschäftshaus", "size":"m"}}
     ]
   },
   {
@@ -39,13 +38,11 @@ export default {
     "title": "Grund für die Bewertung",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"s"}},
-      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"s"}},
-      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"s"}},
-      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"s"}},
-      {  id:"4", value:"Finanzamt", "next" : 998, "prev" : -1, "options": {"icon":"finance_office.svg", "text":"Finanzamt", "size":"s"}},
-      {  id:"5", value:"Gericht", "next" : 998, "prev" : -1, "options": {"icon":"law_court_justice.svg", "text":"Gericht", "size":"s"}},
-      {  id:"6", value:"Sonstiges", "next" : 3, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"s"}},
+      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"m"}},
+      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"m"}},
+      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"m"}},
+      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"m"}},
+      {  id:"4", value:"Sonstiges", "next" : 3, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"m"}},
     ]
   },
   {
@@ -91,13 +88,11 @@ export default {
     "title": "Wählen Sie den Grund für eine Immobilienbewertung aus",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"s"}},
-      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"s"}},
-      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"s"}},
-      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"s"}},
-      {  id:"4", value:"Finanzamt", "next" : 998, "prev" : -1, "options": {"icon":"finance_office.svg", "text":"Finanzamt", "size":"s"}},
-      {  id:"5", value:"Gericht", "next" : 998, "prev" : -1, "options": {"icon":"law_court_justice.svg", "text":"Gericht", "size":"s"}},
-      {  id:"6", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"s"}},
+      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"m"}},
+      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"m"}},
+      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"m"}},
+      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"m"}},
+      {  id:"4", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"m"}},
     ]
   },
   {
@@ -127,11 +122,11 @@ export default {
     "title": "Bitte wählen Sie die Nutzungsart aus",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Produktion", "next" : 202, "prev" : -1, "options": {"icon":"production.svg", "text":"Produktion", "size":"s"}},
-      {  id:"1", value:"Lager/ Logistik", "next" : 202, "prev" : -1, "options": {"icon":"store_logistic.svg", "text":"Lager/ Logistik", "size":"s"}},
-      {  id:"2", value:"Büro/ Praxis", "next" : 202, "prev" : -1, "options": {"icon":"doctors_office.svg", "text":"Büro/ Praxis", "size":"s"}},
-      {  id:"3", value:"Handel", "next" : 202, "prev" : -1, "options": {"icon":"commerce.svg", "text":"Handel", "size":"s"}},
-      {  id:"4", value:"Sonstiges (Eingabefeld)", "next" : 202, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges (Eingabefeld) ", "size":"s"}},      
+      {  id:"0", value:"Produktion", "next" : 202, "prev" : -1, "options": {"icon":"production.svg", "text":"Produktion", "size":"m"}},
+      {  id:"1", value:"Lager/ Logistik", "next" : 202, "prev" : -1, "options": {"icon":"store_logistic.svg", "text":"Lager/ Logistik", "size":"m"}},
+      {  id:"2", value:"Büro/ Praxis", "next" : 202, "prev" : -1, "options": {"icon":"doctors_office.svg", "text":"Büro/ Praxis", "size":"m"}},
+      {  id:"3", value:"Handel", "next" : 202, "prev" : -1, "options": {"icon":"commerce.svg", "text":"Handel", "size":"m"}},
+      {  id:"4", value:"Sonstiges (Eingabefeld)", "next" : 202, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges (Eingabefeld) ", "size":"m"}},      
     ]
   },
   {
@@ -141,13 +136,11 @@ export default {
     "title": "Wählen Sie den Grund für eine Immobilienbewertung aus",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"s"}},
-      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"s"}},
-      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"s"}},
-      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"s"}},
-      {  id:"4", value:"Finanzamt", "next" : 998, "prev" : -1, "options": {"icon":"finance_office.svg", "text":"Finanzamt", "size":"s"}},
-      {  id:"5", value:"Gericht", "next" : 998, "prev" : -1, "options": {"icon":"law_court_justice.svg", "text":"Gericht", "size":"s"}},
-      {  id:"6", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"s"}},
+      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"m"}},
+      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"m"}},
+      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"m"}},
+      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"m"}},
+      {  id:"4", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"m"}},
     ]
   },
   {
@@ -177,13 +170,11 @@ export default {
     "title": "Wählen Sie den Grund für eine Immobilienbewertung aus",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"s"}},
-      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"s"}},
-      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"s"}},
-      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"s"}},
-      {  id:"4", value:"Finanzamt", "next" : 998, "prev" : -1, "options": {"icon":"finance_office.svg", "text":"Finanzamt", "size":"s"}},
-      {  id:"5", value:"Gericht", "next" : 998, "prev" : -1, "options": {"icon":"law_court_justice.svg", "text":"Gericht", "size":"s"}},
-      {  id:"6", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"s"}},
+      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"m"}},
+      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"m"}},
+      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"m"}},
+      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"m"}},
+      {  id:"4", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"m"}},
     ]
   },
   {
@@ -213,11 +204,11 @@ export default {
     "title": "Um welche Nutzungsart handelt es sich?",
     "fieldName": "specialPropertyType",
     "options": [
-      {  id:"0", value:"Hotel", "next" : 403, "prev" : -1, "options": {"icon":"hotel.svg", "text":"Hotel", "size":"s"}},
-      {  id:"1", value:"Pflegeheim", "next" : 403, "prev" : -1, "options": {"icon":"residential_care.svg", "text":"Pflegeheim", "size":"s"}},
-      {  id:"2", value:"Krankenhaus", "next" : 403, "prev" : -1, "options": {"icon":"hospital_2.svg", "text":"Krankenhaus", "size":"s"}},
-      {  id:"3", value:"Gastronomie", "next" : 403, "prev" : -1, "options": {"icon":"gastronomy.svg", "text":"Gastronomie", "size":"s"}},
-      {  id:"4", value:"Sonstiges (Eingabefeld)", "next" : 402, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges (Eingabefeld) ", "size":"s"}},      
+      {  id:"0", value:"Hotel", "next" : 403, "prev" : -1, "options": {"icon":"hotel.svg", "text":"Hotel", "size":"m"}},
+      {  id:"1", value:"Pflegeheim", "next" : 403, "prev" : -1, "options": {"icon":"residential_care.svg", "text":"Pflegeheim", "size":"m"}},
+      {  id:"2", value:"Krankenhaus", "next" : 403, "prev" : -1, "options": {"icon":"hospital_2.svg", "text":"Krankenhaus", "size":"m"}},
+      {  id:"3", value:"Gastronomie", "next" : 403, "prev" : -1, "options": {"icon":"gastronomy.svg", "text":"Gastronomie", "size":"m"}},
+      {  id:"4", value:"Sonstiges (Eingabefeld)", "next" : 402, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges (Eingabefeld) ", "size":"m"}},      
     ]
   },
 
@@ -244,13 +235,11 @@ export default {
     "title": "Wählen Sie den Grund für eine Immobilienbewertung aus",
     "fieldName": "contractReason",
     "options": [
-      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"s"}},
-      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"s"}},
-      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"s"}},
-      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"s"}},
-      {  id:"4", value:"Finanzamt", "next" : 998, "prev" : -1, "options": {"icon":"finance_office.svg", "text":"Finanzamt", "size":"s"}},
-      {  id:"5", value:"Gericht", "next" : 998, "prev" : -1, "options": {"icon":"law_court_justice.svg", "text":"Gericht", "size":"s"}},
-      {  id:"6", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"s"}},
+      {  id:"0", value:"Erbe", "next" : 998, "prev" : -1, "options": {"icon":"heritage.svg", "text":"Erbe", "size":"m"}},
+      {  id:"1", value:"Scheidung", "next" : 998, "prev" : -1, "options": {"icon":"divorce.svg", "text":"Scheidung", "size":"m"}},
+      {  id:"2", value:"Verkauf", "next" : 998, "prev" : -1, "options": {"icon":"sell.svg", "text":"Verkauf", "size":"m"}},
+      {  id:"3", value:"Kauf", "next" : 998, "prev" : -1, "options": {"icon":"buy.svg", "text":"Kauf", "size":"m"}},
+      {  id:"4", value:"Sonstiges", "next" : 998, "prev" : -1, "options": {"icon":"other.svg", "text":"Sonstiges", "size":"m"}},
     ]
   },
   {
