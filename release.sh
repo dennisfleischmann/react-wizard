30a4d9406ebe1d206188d39f84b7ff8ba5b06142
Dennis Fleischmann <dennisfleischmann@Denniss-MacBook-Pro.local>
2019-12-16 23:21:47 +0530

    Finalized release.sh script

--- release.sh
+++ release.sh
@@ -1,10 +1,28 @@
-mkdir -p packages/heid/release-v1/next-release/img && cp -r build/libs/wizard/img/ packages/heid/release-v1/next-release/img
-cp -r build/libs/wizard/svg/ packages/heid/release-v1/next-release/svg
-cp -r build/static/css/ packages/heid/release-v1/next-release/css
-cp -r build/static/js/ packages/heid/release-v1/next-release/js
+mkdir -p packages/heid/release/next-release/img && cp -r build/libs/wizard/img/ packages/heid/release/next-release/img
+cp -r build/libs/wizard/svg/ packages/heid/release/next-release/svg
+cp -r build/static/css/ packages/heid/release/next-release/css
+cp -r build/static/js/ packages/heid/release/next-release/js
 
 #get version
+VERSION='';
+re="\"(version)\": \"([^\"]*)\"";
+
+while read -r l; do
+    if [[ $l =~ $re ]]; then
+        value="${BASH_REMATCH[2]}";
+        VERSION="$value";
+    fi
+done < package.json;
+
+echo $VERSION;
 
 #Compress
-cd ./packages/heid/release-v1/next-release/
-zip -r -X release-v1.0.1.zip ./
+cd ./packages/heid/release/next-release/
+zip -r -X release-v$VERSION.zip ./
+cd ..
+cd ..
+cd ..
+cd ..
+cp -r packages/heid/release/next-release/release-v$VERSION.zip packages/heid/release/
+rm -r packages/heid/release/next-release/
+
