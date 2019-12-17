64c7d8d5329ed1e0e3cf37922bba59a27dc8bd9f
Tushar M <tushar.mangukiya@innovalabs.tech>
2019-12-17 15:29:07 +0530

    - Confirmation step title align made in center

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -264,7 +264,7 @@ module.exports = function(webpackEnv) {
 
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
-      
+
     },
     resolve: {
       // This allows you to set a fallback for where Webpack should look for modules.
@@ -324,7 +324,7 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -357,7 +357,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -397,7 +397,7 @@ module.exports = function(webpackEnv) {
                 ],
                 cacheDirectory: true,
                 cacheCompression: isEnvProduction,
-                
+
                 // If an error happens in a package, it's possible to be
                 // because it was compiled. Thus, we don't want the browser
                 // debugger to show the original code. Instead, the code
@@ -493,7 +493,7 @@ module.exports = function(webpackEnv) {
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
-      !isEnvProduction ?
+      isEnvProduction ?
       new HtmlWebpackPlugin(
         Object.assign(
           {},
@@ -513,7 +513,7 @@ module.exports = function(webpackEnv) {
               minifyURLs: true,
             },
           },
-          
+
         )
       ): undefined ,
       // Inlines the webpack runtime script. This script is too small to warrant
