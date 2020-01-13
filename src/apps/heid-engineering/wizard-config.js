03df553e046d5f1a2f51dcd31efce014be661dad
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2020-01-13 13:23:44 +0530

    Added content to city-page

--- src/apps/heid-engineering/wizard-config.js
+++ src/apps/heid-engineering/wizard-config.js
@@ -14,14 +14,15 @@ export default {
       "id": 0,
       "type": "cards-step",
       "description":"house select",
-      "title": "Kostenlose & Unverbindliche Erstberatung",
+      "title": "Kostenlose Erstberatung mit Experten",
       "fieldName": "Objekt_Typ",
       "options": [
-        {  id:"0", value:"Tragwerksplanung", "next" : 1, "stepRange":{"from": 1, "to": 99}, "options": {"icon":"/images/wizard/beam.svg", "text":"Tragwerksplanung", "size":"s"}},
-        {  id:"1", value:"Bautechnische Prüfung", "next" : 100, "stepRange":{"from": 100, "to": 199}, "options": {"icon":"/images/wizard/ruler.svg", "text":"Bautechnische Prüfung", "size":"s"}},
-        {  id:"2", value:"Ausführplanung", "next" : 200, "stepRange":{"from": 200, "to": 299}, "options": {"icon":"/images/wizard/planning.svg", "text":"Ausführplanung", "size":"s"}},
-        {  id:"2", value:"Sachverständigen Gutachten", "next" : 300, "stepRange":{"from": 300, "to": 399}, "options": {"icon":"/images/wizard/assessment.png", "text":"Sachverständigen Gutachten", "size":"s"}},
-        {  id:"2", value:"Bauberatung", "next" : 400, "stepRange":{"from": 400, "to": 499}, "options": {"icon":"/images/wizard/consultation.svg", "text":"Beratung", "size":"s"}}
+        {  id:"0", value:"Trag&shy;werks&shy;planung", "next" : 1, "stepRange":{"from": 1, "to": 99}, "options": {"icon":"/images/wizard/beam.svg", "text":"Tragwerksplanung", "size":"s"}},
+        {  id:"1", value:"Bau&shy;technische Prü&shy;fung", "next" : 100, "stepRange":{"from": 100, "to": 199}, "options": {"icon":"/images/wizard/ruler.svg", "text":"Bautechnische Prüfung", "size":"s"}},
+        {  id:"2", value:"Ausführ&shy;planung", "next" : 200, "stepRange":{"from": 200, "to": 299}, "options": {"icon":"/images/wizard/planning.svg", "text":"Ausführplanung", "size":"s"}},
+        {  id:"3", value:"Bauschäden", "next" : 300, "stepRange":{"from": 300, "to": 399}, "options": {"icon":"/images/wizard/assessment.png", "text":"Sachverständigen Gutachten", "size":"s"}},
+        {  id:"4", value:"Bau&shy;beratung", "next" : 400, "stepRange":{"from": 400, "to": 499}, "options": {"icon":"/images/wizard/consultation.svg", "text":"Beratung", "size":"s"}},
+        {  id:"4", value:"Architektur&shy;leistungen", "next" : 500, "stepRange":{"from": 500, "to": 599}, "options": {"icon":"/images/wizard/architect.svg", "text":"Beratung", "size":"s"}}
       ]
     },
     {
